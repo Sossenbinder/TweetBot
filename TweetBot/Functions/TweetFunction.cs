8761e1fea0321bf3241ca796f3f05d975897004c
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-02-08 23:30:24 +0100

    Added retweet capabilities

--- TweetBot/Functions/TweetFunction.cs
+++ TweetBot/Functions/TweetFunction.cs
@@ -1,15 +1,38 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Polly;
+using Polly.Retry;
 using TweetBot.Trigger;
+using Tweetinvi;
+using Tweetinvi.Exceptions;
 using Tweetinvi.Models.V2;
 
 namespace TweetBot.Functions
 {
-	public static class TweetFunction
+	public class TweetFunction
 	{
-		[FunctionName("HandleTweet")]
-		public static async Task HandleTweet([TwitterTrigger("dotnet", "%ConsumerKey%", "%ConsumerKeySecret%", "%AccessToken%", "%AccessTokenSecret%")] TweetV2 tweet)
+		private readonly ITwitterClient _twitterClient;
+
+		private static readonly AsyncRetryPolicy _retryPolicy = Policy
+			.Handle<TwitterException>()
+			.WaitAndRetryAsync(new[]
+			{
+				TimeSpan.FromSeconds(5),
+				TimeSpan.FromSeconds(15),
+				TimeSpan.FromSeconds(30),
+			});
+
+		public TweetFunction(ITwitterClient twitterClient)
 		{
+			_twitterClient = twitterClient;
+		}
+
+		[FunctionName("HandleTweet")]
+		public async Task HandleTweet(
+			[TwitterTrigger("dotnet", "%ConsumerKey%", "%ConsumerKeySecret%", "%AccessToken%", "%AccessTokenSecret%")] TweetV2 tweet)
+		{
+			await _retryPolicy.ExecuteAsync(() => _twitterClient.Tweets.PublishRetweetAsync(long.Parse(tweet.Id)));
 		}
 	}
 }
---
+++ TweetBot/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Tweetinvi;
+
+[assembly: FunctionsStartup(typeof(TweetBot.Startup))]
+
+namespace TweetBot
+{
+	public class Startup : FunctionsStartup
+	{
+		public override void Configure(IFunctionsHostBuilder builder)
+		{
+			var configuration = builder.GetContext().Configuration;
+
+			builder.Services.AddSingleton<ITwitterClient>(new TwitterClient(
+				configuration["ConsumerKey"],
+				configuration["ConsumerKeySecret"],
+				configuration["AccessToken"],
+				configuration["AccessTokenSecret"]));
+		}
+	}
+}
